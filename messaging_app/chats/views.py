1dd0a76c3933e250a92feb318c6482f194e81213
Evidence Uche Aniowoh <aniowohevidence@gmail.com>
2025-06-02 08:59:10 +0100

    views url

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status, filters
+from rest_framework.response import Response
 from .models import Conversation, Message
 from .serializers import ConversationSerializer, MessageSerializer
 
@@ -9,7 +10,9 @@ from .serializers import ConversationSerializer, MessageSerializer
 class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all().prefetch_related('participants', 'messages')
     serializer_class = ConversationSerializer
-    permission_classes = [permissions.AllowAny]  # Customize as needed
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['participants__username']
 
     def get_queryset(self):
         user = self.request.user
@@ -17,11 +20,19 @@ class ConversationViewSet(viewsets.ModelViewSet):
             return self.queryset.filter(participants=user)
         return self.queryset.none()
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all().select_related('conversation', 'sender')
     serializer_class = MessageSerializer
-    permission_classes = [permissions.AllowAny]  # Customize as needed
+    permission_classes = [permissions.AllowAny]
+    filter_backends = [filters.OrderingFilter]
+    ordering = ['sent_at']
 
     def get_queryset(self):
         conversation_id = self.request.query_params.get('conversation_id')
@@ -29,3 +40,8 @@ class MessageViewSet(viewsets.ModelViewSet):
             return self.queryset.filter(conversation__conversation_id=conversation_id)
         return self.queryset.none()
 
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
