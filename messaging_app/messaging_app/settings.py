a3bc518539b1e42bdc0623d61ccf97d587469271
Evidence Uche Aniowoh <aniowohevidence@gmail.com>
2025-06-02 09:15:23 +0100

    installed apps

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'messaging_app.chats',
+    'messaging_app.users',
 ]
 
 REST_FRAMEWORK = {
